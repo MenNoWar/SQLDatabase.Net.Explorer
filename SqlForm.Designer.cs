5b1f2c6844ecd5f42a202549aa2fb63f2a800723
Paul Kaatz <mennowar@outlook.com>
2019-09-25 11:26:45 +0200

    changed button title in sql form added error handling when opening a database view

--- SqlForm.Designer.cs
+++ SqlForm.Designer.cs
@@ -82,7 +82,7 @@
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
             this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Text = "Run Sql Command";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // cbConnections
